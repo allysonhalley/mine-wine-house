2cfd7939d1010b5e2265453821089416d37cf567
Allyson Halley <allysonhalley@gmail.com>
2021-10-05 00:13:14 -0300

    register router ok

--- packages/server/src/controllers/authController.js
+++ packages/server/src/controllers/authController.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcryptjs = require('bcryptjs');
 
 const User = require('../models/User');
 
@@ -8,7 +9,7 @@ router.post('/register', async (req, res) => {
   const { email } = req.body;
 
   try {
-    if (await User.findOne({ email: email }) ){      
+    if (await User.findOne({ email }) ){      
       return res.status(400).send({ error: 'Email already register!'});
     }    
     const user = await User.create(req.body);
@@ -21,19 +22,30 @@ router.post('/register', async (req, res) => {
   }
 });
 
-router.get('/users', async (req, res) => {
-  try {    
-    
-    const users = await User.find();
-    users.password = undefined;
+router.post('/authenticate', async (req, res) => {
+  const { email, password } = req.body;
 
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user){
+    return res.status(400).send({ error: 'User not found!'});
+  }
+
+  if (!await bcryptjs.compare(password, user.password)){
+    return res.status(400).send({ error: 'Invalid password.'});
+  }
+
+  res.send({ user });
+});
+
+router.get('/users', async (req, res) => {
+  try {        
+    const users = await User.find();
     return res.send({ users });
 
   } catch (err) {
     return res.status(400).send({ error: 'Ops!'})  ;  
   }
-
-
 });
 
 module.exports = app => app.use('/auth', router);
